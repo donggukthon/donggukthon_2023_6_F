5eb63b73905628e5cb9157936f1d3e48e58211b3
minseong0324 <kwan0324@naver.com>
2023-12-18 00:22:39 +0900

    Feat : Router.tsx 추가

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import {Route, Routes} from 'react-router-dom';
+export default function Router() {
+
+  return (
+    <>
+      <Routes>
+        {/*<Route path="/" element={<Home />} />*/}
+        
+      </Routes>
+    </>
+  );
+}
