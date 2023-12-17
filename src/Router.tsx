ae373645cbd4ed1bd2fae50892badd031f0c8606
minseong0324 <kwan0324@naver.com>
2023-12-18 08:55:06 +0900

    Feat : 제보하기 등록 페이지 UI 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import {Route, Routes} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Report from './pages/Report/Report';
+import ReportUpload from './pages/ReportUpload/ReportUpload';
 export default function Router() {
 
   return (
@@ -8,6 +9,7 @@ export default function Router() {
         <Routes>
             <Route path="/home" element={<Home />} />
             <Route path="/report" element={<Report />} />
+            <Route path="/report/upload" element={<ReportUpload />} />
         </Routes>
     </>
   );
---
+++ src/interfaces/reportUpload.ts
@@ -0,0 +1,3 @@
+export interface ImagePreviewProps {
+    imageUrl: string;
+  }
