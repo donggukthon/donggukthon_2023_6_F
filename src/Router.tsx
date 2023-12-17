34e076edaf4520d11f9602613cc2c45d123a8f6d
minseong0324 <kwan0324@naver.com>
2023-12-18 07:04:24 +0900

    Feat : report page route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,14 @@
 import {Route, Routes} from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Report from './pages/Report/Report';
 export default function Router() {
 
   return (
     <>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        
-      </Routes>
+        <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/report" element={<Report />} />
+        </Routes>
     </>
   );
 }
