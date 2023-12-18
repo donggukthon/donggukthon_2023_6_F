8d8a13381e1478b9061104be3708c55ee4825e4e
minseong0324 <kwan0324@naver.com>
2023-12-19 07:09:59 +0900

    Feat : api instance 추가

---
+++ src/apis/axios.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+//import useSetTokens from '../hooks/useSetTokens';
+
+const getAccessTokenFromCookies = () => Cookies.get('accessCookie');
+//const getRefreshTokenFromCookies = () => Cookies.get('refreshCookie');
+
+export const instance = axios.create({
+  baseURL: 'https://cleancity.site', //TODO: 도메인 사면 수정
+  headers: {
+    Authorization: `Bearer ${getAccessTokenFromCookies()}`,
+  },
+});
+
+instance.interceptors.request.use(
+  config => {
+    config.headers.Authorization = `Bearer ${getAccessTokenFromCookies()}`;
+    return config;
+  },
+  error => Promise.reject(error)
+);
