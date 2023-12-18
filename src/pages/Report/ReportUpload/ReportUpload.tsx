72c9336ddc943c97991ba35a181b01ea53e688cc
minseong0324 <kwan0324@naver.com>
2023-12-18 20:53:58 +0900

    Feat : 이미지 인식 ai 추가

--- src/pages/Report/ReportUpload/ReportUpload.tsx
+++ src/pages/Report/ReportUpload/ReportUpload.tsx
@@ -5,14 +5,15 @@ import PageLayoutGreenBottom from '@/components/PageLayoutGreenBottom/PageLayout
 import CheckButtonImg from '@/assets/Button/CheckButton.png';
 import * as S from './style';
 import useInput from '@/hooks/useInput';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 export default function ReportUpload() {
   const location = useLocation();
   const imageFile = location.state?.image;
   const [imageUrl, setImageUrl] = useState(null);
+  const [prediction, setPrediction] = useState('');
   const content = useInput<HTMLTextAreaElement>();
 
-  // 이미지 파일이 변경될 때만 URL 업데이트
   useEffect(() => {
     if (imageFile) {
       const newImageUrl = URL.createObjectURL(imageFile);
@@ -21,13 +22,32 @@ export default function ReportUpload() {
       // URL 사용 후 해제
       return () => URL.revokeObjectURL(newImageUrl);
     }
-  }, []);
+  }, [imageFile]);
+
+  useEffect(() => {
+    const classifyImage = async (imageSrc) => {
+      const net = await mobilenet.load();
+      const imgElement = document.createElement('img');
+      imgElement.src = imageSrc;
+      imgElement.onload = async () => {
+        const result = await net.classify(imgElement);
+        setPrediction(result[0].className);
+      };
+    };
+
+    if (imageUrl) {
+      classifyImage(imageUrl);
+    }
+  }, [imageUrl]);
 
   return (
     <PageLayoutGreen title={"제보하기"}>
       {imageUrl && (
         <S.ImagePreview imageUrl={imageUrl} />
       )}
+      <div>
+        {prediction && <p>Prediction: {prediction}</p>}
+      </div>
       <S.ContentsArea
         placeholder="쓰레기통 위치에 대한 간단한 설명."
         value={content.value}
