b6ac0aacc725ce2abbf43192e8460789f690209b
minseong0324 <kwan0324@naver.com>
2023-12-20 08:49:41 +0900

    Feat : 쓰레기 없는지 detect

--- src/atoms/trash.ts
+++ src/atoms/trash.ts
@@ -5,8 +5,18 @@ import { atom } from "recoil";
 export const trashesState = atom<ITrashesLocation>({
     key: 'trashesState',
     default: {
-      data: {
-        complaintList: []
-      }
+        data: {
+            complaintList: []
+        }
     }
-  });
+});
+
+export const imageFileState = atom<File>({
+    key: 'imageFileState',
+    default: null, // 초기값을 null 또는 빈 문자열로 설정
+});
+
+export const imageUrlState = atom<string>({
+    key: 'imageUrlState', // 고유한 key
+    default: '', // 초기값
+});
--- src/components/Modal/TrashModal/TrashModal.tsx
+++ src/components/Modal/TrashModal/TrashModal.tsx
@@ -1,24 +1,53 @@
+// TrashModal.tsx
+import React, { useRef, useState } from 'react';
 import Modal from '../Modal';
 import ModalButton from '@/components/Button/ModalButton/ModalButton';
 import * as S from './style';
 import useModal from '@/hooks/useModal';
 import SmallModal from '../SmallModal/SmallModal';
-import {useMutation} from '@tanstack/react-query';
-import { trashesState } from '@/atoms/trash';
-import { useRecoilValue } from 'recoil';
+import { useMutation } from '@tanstack/react-query';
 import { cleanTrash } from '@/apis/trash';
+import { useRecoilState } from 'recoil';
+import { imageFileState, imageUrlState } from '@/atoms/trash';
+import ImageAnalyzer from '@/components/GoogleVision/ImageAnalyzer';
 
 type TrashModalProps = {
   onClose: () => void;
   isOpen: boolean;
-  trashId: number; // 여기에서 trashId의 타입을 number로 명시
+  trashId: number;
 };
 
 export default function TrashModal({ onClose, isOpen, trashId }: TrashModalProps) {
-  const { isOpen : openSucessModal, openModal, closeModal } = useModal(); // useModal 훅 사용
-  const trashes = useRecoilValue(trashesState).data.complaintList;
-  const selectedTrash = trashes.find(trashes => trashes.trashId === trashId);
+  const [, setImageFile] = useRecoilState(imageFileState);
+  const [imageUrl, setImageUrl] = useRecoilState(imageUrlState);
+  const inputRef = useRef(null);
+  const [, setIsTrashDetected] = useState(true); // 기본적으로 쓰레기가 있다고 가정
+  const { isOpen: openSuccessModal, openModal, closeModal } = useModal();
 
+  // 이미지 분석 결과 처리
+  const handleDetectionResult = (labels) => {
+    const trashRelatedKeywords = ['trash', 'garbage', 'waste', 'litter', 'rubbish', 'debris', 'refuse', 'pollution', 'dust'];
+    const isTrash = labels.some(label => trashRelatedKeywords.includes(label.toLowerCase()));
+    setIsTrashDetected(isTrash);
+
+    if (!isTrash) {
+      mutate();
+    } else {
+      alert('아직 쓰레기가 더 있어요.');
+    }
+  };
+
+  // 카메라 앱 연결
+  const handleCapture = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setImageFile(file); // Recoil atom을 업데이트
+      const newImageUrl = URL.createObjectURL(file);
+      setImageUrl(newImageUrl);
+    }
+  };
+
+  // 쓰레기 상태 변경 API 호출
   const { mutate } = useMutation({
     mutationFn: () => cleanTrash(trashId),
     onSuccess: () => {
@@ -29,42 +58,48 @@ export default function TrashModal({ onClose, isOpen, trashId }: TrashModalProps
     },
   });
 
-  const handleCleanTrash = () => {
-    //TODO: 쓰레기 치웠어요! 버튼 누르면 카메라앱 연결 -> 쓰레기 없는지 detection -> 없으면 쓰레기 상태변경 api 호출, 있으면 alert(‘아직 쓰레기가 더 있어요.’)
-  }
-
-  const handleCloseSuccessModal = () => {
-    closeModal();
-    onClose();
-  }
-
   return (
     <>
       <Modal
-        modalTitle={selectedTrash ? selectedTrash.address : ''}
+        modalTitle={''}
         isOpen={isOpen}
         onClose={onClose}
         imageType={'MediumModal'}
       >
         <S.Wrapper>
           <S.ImgBox>
-          <S.Img src={selectedTrash ? selectedTrash.picture : ''} />
+            <S.Img src={imageUrl} />
+            <input
+              ref={inputRef}
+              type="file"
+              accept="image/*"
+              capture="environment"
+              onChange={handleCapture}
+              hidden
+            />
           </S.ImgBox>
         </S.Wrapper>
         <S.ModalOkButtonWrapper>
-          <ModalButton buttonName="쓰레기를 치웠어요!" onClick={handleCleanTrash} />
+          <ModalButton buttonName="쓰레기를 치웠어요!" onClick={() => inputRef.current.click()} />
         </S.ModalOkButtonWrapper>
       </Modal>
       
-      {openSucessModal && (
-          <SmallModal 
-            modalTitle={'고마워요!'} 
-            modalText={'항상 환경을 생각하는 여러분 덕분에 \n 더 깨끗한 세상으로 변할거예요!'} 
-            isOpen={isOpen}
-            onClose={handleCloseSuccessModal}
-          />
+      {openSuccessModal && (
+        <SmallModal 
+          modalTitle={'고마워요!'} 
+          modalText={'항상 환경을 생각하는 여러분 덕분에 \n 더 깨끗한 세상으로 변할거예요!'} 
+          isOpen={openSuccessModal}
+          onClose={() => {
+            closeModal();
+            onClose();
+          }}
+        />
+      )}
+
+      {/* 이미지 분석 컴포넌트 */}
+      {imageUrl && (
+        <ImageAnalyzer imageUrl={imageUrl} onDetectionResult={handleDetectionResult} />
       )}
     </>
-
   );
 }
