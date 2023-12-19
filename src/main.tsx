7081444a2a1bf33efc96bf20fd0c47c34561f634
minseong0324 <kwan0324@naver.com>
2023-12-19 09:23:14 +0900

    Feat : recoil root 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import {GlobalStyle} from '@/style';
+import {RecoilRoot} from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <RecoilRoot>
+      <GlobalStyle />
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
 )
